cad3dfb9b06bf964244b5b9848586bee0e417ca2
dollarstorepoetry <nicholasagrudzinski@gmail.com>
2024-12-22 07:24:33 -0600

    Added functionality outside of the directory. - Still not ready to move to /usr/bin.

--- README.md
+++ README.md
@@ -3,11 +3,9 @@
 This program does basically the same thing you expect `fortune` to do, except with monkeytype.com quotes instead of whatever quotes `fortune` uses.
 Pipe into `cowsay` as desired. 
 
-As of right now, this is still a work in progress, not very much ready for anything.
+Currently, if you would like to use this tool, the only way to do so is to clone the repository into the location of your choosing
 
 ## what each file does (idk how to document)
 `monkeyfortune.c` - the source code in questoin
 
 `update-quote-repo.sh` - bash script that updates all the quote repositories 
-
-`fortune.c` - doesn't do anything. this is the original source code for `fortune` but i aint reading allat
--- src/monkeyfortune.c
+++ src/monkeyfortune.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
-// #include <cjson/cJSON.h>
 #include "cJSON.h"
-// #include <cJSON.h>
 #include <time.h>  // just for random :^)
+#include <libgen.h> // for getting the directory
+#include <unistd.h> // for getting the directory
+#include <string.h> // strcat
+
+void get_binary_directory(char *buffer, size_t size) {
+    char path[1024];
+    ssize_t len = readlink("/proc/self/exe", path, sizeof(path) - 1);
+
+    if (len == -1) {
+        fprintf(stderr, "readlink ???");
+        exit(1);
+    }
+
+    path[len] = '\0'; // Null-terminate the path
+
+    // Extract the directory using dirname
+    char *dir = dirname(path);
+    snprintf(buffer, size, "%s", dir);
+}
 
 int main(){
-	FILE *quotefp = fopen("monkeytype-quotes/english.json", "r");
+	char dir[1024];
+	get_binary_directory(dir, sizeof(dir));
+	// FILE *quotefp = fopen("monkeytype-quotes/english.json", "r");
+	FILE *quotefp = fopen(strcat(dir, "/monkeytype-quotes/english.json"), "r");
+	// TODO: make this directory-agnostic ???? idk that makes me sound smart
+	// TODO: make this work with windows (although who's using this on windows tbh)
 	if (quotefp == NULL) { 
 		printf("Error: Unable to open the file.\n");
 		return 1; 
    	}
-
-	//fseek(quotefp, 0, SEEK_END);
-	//unsigned long long file_size = ftell(quotefp);
-	//rewind(quotefp);
 
 	// read contents from file
 	char *buffer = (char *)malloc(1 << 23); // 2^23 should be big enough to store this file forever.
@@ -22,40 +40,56 @@ int main(){
 	unsigned long long len = fread(buffer, 1, 1 << 23, quotefp);
 	fclose(quotefp); 
 
-	/* grab random quote from file and output */
- 	cJSON *json = cJSON_Parse(buffer); 	
-	free(buffer); // should probably be able to free the buffer atp. delete this comment later
+ 	// initialize json parser
+	cJSON *json = cJSON_Parse(buffer); 	
+	free(buffer); 
 	if (json == NULL) { 
 		const char *error_ptr = cJSON_GetErrorPtr(); 
 		if (error_ptr != NULL) { 
-			printf("Error: %s\n", error_ptr); // what the hell did i write 3 months ago
+			fprintf(stderr, "Error: %s\n", error_ptr);
 		} 
 		cJSON_Delete(json); 
-		return 1; 
+		exit(1);
 	} 
 
+	// loop through the ENTIRE quote array and store info in respective arrays
+	// TODO: can this be more efficient? lol
+	// TODO: add support for multiple languages
 	cJSON *quotes = cJSON_GetObjectItemCaseSensitive(json, "quotes");
-	char** quote_text = (char **)malloc((len+1) * sizeof(char *));
-	char** author_info = (char **)malloc((len+1) * sizeof(char *));
+	char** quote_text = (char **)malloc((len+1) * sizeof(char *));  // array of quotes
+	char** author_info = (char **)malloc((len+1) * sizeof(char *)); // array of sources
 	int nq = 0;
 	if (cJSON_IsArray(quotes)){
 		cJSON *quote_obj;
 		cJSON_ArrayForEach(quote_obj, quotes){
-			// wtf is error checking
-			char* text = cJSON_GetObjectItemCaseSensitive(quote_obj, "text")->valuestring;
-			char* source = cJSON_GetObjectItemCaseSensitive(quote_obj, "source")->valuestring;
-			quote_text[nq] = text;
-			author_info[nq] = source;
+			cJSON* text = cJSON_GetObjectItemCaseSensitive(quote_obj, "text");
+			cJSON* source = cJSON_GetObjectItemCaseSensitive(quote_obj, "source");
+
+			if (!cJSON_IsString(text)){
+				fprintf(stderr, "Quote text is not recognized as valid text.");
+				exit(1);
+			}
+			else quote_text[nq] = text->valuestring;
+
+			if (!cJSON_IsString(source)){
+				fprintf(stderr, "Quote author is not recognized as valid text.");
+				exit(1);
+			}
+			else author_info[nq] = source->valuestring;
+			
 			nq++;
 		}
+	} else {
+		fprintf(stderr, "quotes object not recognized as array.");
+		exit(1);
 	}
 
+	// finally, pick a random quote from our. giant. array and output
 	srand(time(NULL));
 	int ridx = rand() % nq;
-	// printf("%d\n", ridx);
 	printf("\"%s\"\n\t-%s\n", quote_text[ridx], author_info[ridx]);
 
-	// delete the JSON object and free ya shit
+	// delete the JSON object and free all ya stuff.
 	cJSON_Delete(json); 
 	free(quote_text);
 	free(author_info);
